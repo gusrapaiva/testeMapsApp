89a8273cda6dfcc27542d367a11ca7a98a634203
gusrapaiva <gustavorapaiva@gmail.com>
2024-03-25 20:16:29 -0300

    localizacao atual

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+} from 'expo-location';
 
 export default function App() {
+
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  async function requestLocationPermissions(){
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if(granted){
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+      
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        location && 
+        <MapView style={styles.map} 
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
+          }}
+        >
+          <Marker 
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        </MapView>
+      }
+    
     </View>
   );
 }
@@ -13,8 +53,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
 });
