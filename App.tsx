6f96197c4961c429b83968eb6a189e68b14c2c0c
gusrapaiva <gustavorapaiva@gmail.com>
2024-03-26 15:15:31 -0300

    identifica os psicólogos próximos

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { LatLng, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import {
   requestForegroundPermissionsAsync,
@@ -12,8 +12,8 @@ import {
 export default function App() {
 
   const [location, setLocation] = useState<LocationObject | null>(null);
-
   const mapRef = useRef<MapView>(null);
+  const [results, setResults] = useState<any[]>([]);
 
   async function requestLocationPermissions(){
     const { granted } = await requestForegroundPermissionsAsync();
@@ -21,10 +21,9 @@ export default function App() {
     if(granted){
       const currentPosition = await getCurrentPositionAsync();
       setLocation(currentPosition);
-      
+      searchPlaces();
     }
   }
-
 
   useEffect(() => {
     requestLocationPermissions();
@@ -42,6 +41,42 @@ export default function App() {
       })
     });
   }, []);
+
+  const searchPlaces = async () => {
+    const googleApisUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json";
+    const input = "Psicologo";
+    const locationS = `${location?.coords.latitude}, ${location?.coords.longitude}&radius=2000`;
+    const url = `${googleApisUrl}?query=${input}&location=${locationS}&key=AIzaSyBajmU9dXONFjV2TALolSfJlSetCSywIQs`;
+    try{
+      const resp = await fetch(url);
+      const json = await resp.json();
+      // console.log(json);
+      if(json && json.results){
+        const coords: LatLng[] = []
+        for(const item of json.results) {
+          coords.push({
+            latitude: item.geometry.location.lat,
+            longitude: item.geometry.location.lng,
+          })
+        }
+        setResults(json.results)
+        if(coords.length){
+          mapRef.current?.fitToCoordinates(coords, {
+            edgePadding: {
+              top: 10,
+              right: 10,
+              bottom: 10,
+              left: 10
+            },
+            animated: true
+          })
+        }
+      }
+    }
+    catch(e){
+      console.error(e)
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -62,6 +97,14 @@ export default function App() {
               longitude: location.coords.longitude,
             }}
           />
+
+          {results.length ? results.map((item, i) => {
+            const coord: LatLng = {
+              latitude: item.geometry.location.lat,
+              longitude: item.geometry.location.lng,
+            }
+            return <Marker key={`search-item-${i}`} coordinate={coord} title={item.name} description="" />
+          }): null}
         </MapView>
       }
     
